770c4b61f4898d71d77bfa24cc9836a7294e7502
Jorge-Feo <Jorge-Feo@pclumi2.imse-cnm.csic.es>
2023-10-17 22:22:38 +0200

    Parte inicial del proyecto(primera etrega)

---
+++ Proyecto_Jorge_1/lab/funciones/funciones.py
@@ -0,0 +1,78 @@
+'''
+Created on 15 oct 2023
+
+@author: Jorge-Feo
+'''
+"""
+Ejercicio 1
+"""
+def es_primo(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input("Inserte un número entero y positivo: "))
+
+if es_primo(n):
+    print("El {} es un número primo".format(n))
+else:
+    print("El {} no es un número primo".format(n))
+"""
+Ejercicio 2
+"""
+from math import factorial
+def num_combinatorio(n,k):
+    if n >= k:
+        return factorial(n)//(factorial(k)*factorial(n-k))
+    else:
+        return("n debe ser mayor a k para poder realizar la operación")
+n= int(input("Inserte el primer número entero, siendo el primero mayor que el segundo: "))   
+k= int(input("Inserte el segundo número entero, siendo el primero mayor que el segundo: "))  
+print ("El resultado del numero combinatorio de {} y {} es {}".format(n,k,num_combinatorio(n,k)))
+        
+"""
+Ejercicio 3
+"""       
+def funcion_S(n,k):
+    if n >= k:
+        resultado = 0
+    
+        for i in range(k+1):
+            resultado += ((-1) ** i) * num_combinatorio(k,i) * (2 ** (k - i)) * factorial(k - i)
+    
+            return (1 / factorial(k)) * resultado
+    else:
+        return("n debe ser mayor a k para poder realizar la operación")
+n= int(input("Inserte el primer número entero, siendo el primero mayor que el segundo: "))   
+k= int(input("Inserte el segundo número entero, siendo el primero mayor que el segundo: "))
+"""
+Ejercicio 4
+"""
+def dif_num_lista(lista):
+    for i in range (0, len(lista)):
+        lista[i]= abs(lista[i]-lista[i+1])
+    return lista
+lista=input("Introduzca la lista a Continuación: ")
+"""
+Ejercicio 5
+"""
+def cadena_mas_larga(lista_cadenas):
+    if not lista_cadenas:
+        return None
+    
+    cadena_mas_larga = lista_cadenas[0]
+    
+    for cadena in lista_cadenas:
+        if len(cadena) > len(cadena_mas_larga):
+            cadena_mas_larga = cadena
+    
+    return cadena_mas_larga
+lista_cadenas=input("Introduzca la lista a Continuación: ")
+
---
+++ Proyecto_Jorge_1/lab/test/TestFunciones.py
@@ -0,0 +1,6 @@
+'''
+Created on 15 oct 2023
+
+@author: Jorge-Feo
+'''
+
